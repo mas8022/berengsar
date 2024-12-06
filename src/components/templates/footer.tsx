53eaad0c57aacdf50e76253c577c0cceb94055e4
masproo22 <m.abbaszadeh2002@gmail.com>
2024-12-06 19:18:41 +0330

    fix

---
+++ src/components/templates/footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="w-full h-[30rem] bg-gradient-to-b to-[#84B8AC] from-[#D8E27C] footer-shadow"></div>;
+};
+
+export default Footer;
