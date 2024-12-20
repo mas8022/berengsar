d21d74c033d18dc151c7316ab49305e3243ab7b7
masproo22 <m.abbaszadeh2002@gmail.com>
2024-12-20 11:43:38 +0330

    complete cms/

--- utils/useModal.tsx
+++ utils/useModal.tsx
@@ -1,38 +1,35 @@
-import React, { useState, useCallback, useEffect } from "react";
-
-const generateKey = (): string => {
-  return `modal-${Date.now()}`;
-};
+import React, { useState, useCallback } from "react";
 
 interface ModalProps {
   children: React.ReactNode;
 }
 
+const overlayStyles: React.CSSProperties = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100vw",
+  height: "100vh",
+  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  zIndex: 10000,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "20px"
+};
+
+const modalStyles: React.CSSProperties = {
+  backgroundColor: "rgba(255, 255, 255, 0)",
+  borderRadius: "10px",
+  zIndex: 10001,
+  maxWidth: "500px",
+};
+
 const useModal = () => {
-  const getModalKey = (): string => {
-    let key = localStorage.getItem("modalKey");
-    if (!key) {
-      key = generateKey();
-      localStorage.setItem("modalKey", key);
-    }
-    return key;
-  };
-
-  const key = getModalKey();
-
-  const getInitialState = (): boolean => {
-    const storedState = localStorage.getItem(key);
-    return storedState ? JSON.parse(storedState) : false;
-  };
-
-  const [isOpen, setIsOpen] = useState<boolean>(getInitialState);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const openModal = useCallback(() => setIsOpen(true), []);
   const closeModal = useCallback(() => setIsOpen(false), []);
-
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(isOpen));
-  }, [isOpen, key]);
 
   const Modal: React.FC<ModalProps> = ({ children }) => {
     if (!isOpen) return null;
@@ -41,26 +38,6 @@ const useModal = () => {
       if ((e.target as HTMLDivElement).className === "modal-overlay") {
         closeModal();
       }
-    };
-
-    const overlayStyles: React.CSSProperties = {
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: "100vw",
-      height: "100vh",
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
-      zIndex: 10000,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    };
-
-    const modalStyles: React.CSSProperties = {
-      backgroundColor: "rgba(255, 255, 255, 0)",
-      borderRadius: "10px",
-      zIndex: 10001,
-      maxWidth: "500px",
     };
 
     return (
