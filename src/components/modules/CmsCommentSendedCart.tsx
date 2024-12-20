2db9810ac8df67078900cbbece2124c09a1ebbbe
masproo22 <m.abbaszadeh2002@gmail.com>
2024-12-20 18:53:32 +0330

    fix

--- src/components/modules/CmsCommentSendedCart.tsx
+++ src/components/modules/CmsCommentSendedCart.tsx
@@ -45,7 +45,7 @@ const CmsCommentSendedCart = memo(
             .then((res) => res.json())
             .then((result) => {
               console.log("result: ", result);
-              
+
               if (result.status === 200) {
                 toast.success(result.message);
               } else {
@@ -58,31 +58,34 @@ const CmsCommentSendedCart = memo(
     };
 
     return (
-      <div className="w-[32rem] py-10 bg-first dark:bg-[#0d141f]/50 flex flex-col items-center shadow-md">
-        <p className="text-[15px] font-bold mb-12 dark:text-first">
-          {user?.fullName}
-        </p>
-
+      <div className="w-[32rem] p-8 bg-second flex flex-col items-center justify-between gap-4 shadow-md rounded-lg">
         <div className="w-full flex flex-col items-center gap-6 px-8">
-          <p className="text-[1.3rem] font-light text-black/60 line-clamp-1 self-center dark:text-first/80">
-            {user?.email}
-          </p>
-          <p className="text-[1.4rem] dark:text-first p-6 rounded-lg bg-black/5 dark:bg-black/20">
+          {user?.fullName && (
+            <p className="text-2xl font-bold dark:text-first">
+              {user?.fullName}
+            </p>
+          )}
+          {user?.email && (
+            <p className="text-[1.3rem] font-light text-black/60 line-clamp-1 self-center dark:text-first/80">
+              {user?.email}
+            </p>
+          )}
+          <p className="w-full text-[1.4rem] dark:text-first p-6 rounded-lg bg-black/5 dark:bg-black/20 text-center">
             {comment}
           </p>
-          <div className="w-full flex items-center justify-between gap-2 child:shadow-sm">
-            <div
-              onClick={() => confirmComment(_id)}
-              className="w-full h-14 flex items-center cursor-pointer justify-center text-first text-[1.5rem] font-light border-1 bg-second active:bg-first active:border-second active:text-second rounded-lg"
-            >
-              تایید
-            </div>
-            <div
-              onClick={() => deleteComment(_id)}
-              className="w-full h-14 flex items-center cursor-pointer justify-center text-first text-[1.5rem] font-light border-1 bg-second active:bg-first active:border-second active:text-second rounded-lg"
-            >
-              پاک کردن
-            </div>
+        </div>
+        <div className="w-full flex items-center justify-between gap-2 child:shadow-sm">
+          <div
+            onClick={() => deleteComment(_id)}
+            className="w-full h-14 flex items-center cursor-pointer justify-center text-first text-[1.5rem] font-light border-1 bg-rose-500 active:bg-first active:border-second active:text-second rounded-lg"
+          >
+            پاک کردن
+          </div>
+          <div
+            onClick={() => confirmComment(_id)}
+            className="w-full h-14 flex items-center cursor-pointer justify-center text-first text-[1.5rem] font-light border-1 bg-green-500 active:bg-first active:border-second active:text-second rounded-lg"
+          >
+            تایید
           </div>
         </div>
       </div>
